f17fa72aff6db8bc04181aa588213244e064d034
jbesraa <jbesraa@gmail.com>
2023-05-16 11:46:32 +0300

    add types

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export enum SentryDataPeriods {
+    OneDay = "24h",
+    SevenDays = "7d",
+    TwoWeeks = "14d",
+}
+
+export enum SentryDataTypes {
+    LoginStats = "login",
+    SigningStats = "tx",
+}
+
