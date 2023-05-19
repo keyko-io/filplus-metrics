35c1eb54672f1e9ad6475aee2a900deccec253b1
jbesraa <jbesraa@gmail.com>
2023-05-19 12:18:05 +0300

    temp

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,25 +1,37 @@
-import axios from "axios"
-import { SentryDataTypes, SentryDataPeriods } from "../types"
-import { formatSentryURL } from "../utils"
-import { config } from "../config"
+import axios from "axios";
+import { SentryDataTypes, SentryDataPeriods } from "../types";
+import { formatSentryURL } from "../utils";
+import { config } from "../config";
 
 export const fetchSentryData = async (
   period: SentryDataPeriods,
   type: SentryDataTypes
 ) => {
   try {
-    const url = formatSentryURL(period, type)
-    const response = await axios.get(url)
+    const url = formatSentryURL(period, type);
+    const response = await axios.get(url);
 
-    return response.data
+    return response.data;
   } catch (error) {
-    console.error(error)
-    return {}
+    console.error(error);
+    return {};
   }
-}
+};
+
+export const fetchNewData = async () => {
+  try {
+    const response = await axios.get("http://localhost:4000/stats/old");
+
+    console.log("response.data", response.data)
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
+};
 
 export async function getOpenBugsCount() {
-  const url = `${config.apiUri}/stats/issues`
-  const res = await axios.get(url)
-  return res.data
+  const url = `${config.apiUri}/stats/issues`;
+  const res = await axios.get(url);
+  return res.data;
 }
--- src/routes/Blockchain.tsx
+++ src/routes/Blockchain.tsx
@@ -1,13 +1,35 @@
-import BarChart from "../components/BarChart"
+import { useEffect } from "react";
+import BarChart from "../components/BarChart";
+import { fetchNewData } from "../api";
+
+const RequiredEvents = {
+  "Request Proposed": true,
+  "Proposal Failed": true,
+  "Request Approved": true,
+  "Approval Failed": true,
+  "Github Login Success": true,
+  "Loaded Github Token Successfully": true,
+  "Ledger Login Successful": true,
+  "Ledger Login Failed": true,
+};
 
 const Blockchain = () => {
+  useEffect(() => {
+    const handler = async () => {
+      const res = await fetchNewData();
+      console.log("res", res);
+    };
+
+    handler()
+  }, []);
+
   return (
     <div className="flex flex-col space-y-6">
       <BarChart />
       <BarChart />
       <BarChart />
     </div>
-  )
-}
+  );
+};
 
-export default Blockchain
+export default Blockchain;
