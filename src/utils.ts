c2ea8b9e5e6f477715bb3bb0baea0706618e9375
hcsoylu <hcsoylu.dev@gmail.com>
2023-05-23 18:40:54 +0300

    add utils func for chart

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { SentryDataTypes } from "./types"
+import { BarData, SentryDataTypes } from "./types"
 import { config } from "./config"
 
 export const formatSentryURL = (period: string, type: SentryDataTypes) => {
@@ -13,4 +13,43 @@ export const calculateTotalLastNDays = (
   const values = Object.values(data)
   const lastNDaysValues = values.slice(-n)
   return lastNDaysValues.reduce((acc: number, value: number) => acc + value, 0)
+}
+
+export const barChartOption = (data: BarData[]) => {
+  return {
+    tooltip: {},
+    grid: {
+      left: "5%",
+      right: "5%",
+      top: "10%",
+      bottom: "10%",
+    },
+    xAxis: {
+      type: "category",
+      data: data.map((item) => item.day),
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        name: "Success",
+        stack: "total",
+        type: "bar",
+        data: data.map((item) => item.positive),
+        itemStyle: {
+          color: "#2196F3",
+        },
+      },
+      {
+        name: "Failure",
+        stack: "total",
+        type: "bar",
+        data: data.map((item) => item.negative),
+        itemStyle: {
+          color: "#FF5722",
+        },
+      },
+    ],
+  }
 }
