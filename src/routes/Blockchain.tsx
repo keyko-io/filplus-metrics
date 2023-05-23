38d5523d26132931516727be1d80157c62d06ca5
hcsoylu <hcsoylu.dev@gmail.com>
2023-05-23 18:42:53 +0300

    reafctor code

--- src/routes/Blockchain.tsx
+++ src/routes/Blockchain.tsx
@@ -1,6 +1,8 @@
 import BarChart from "../components/BarChart"
 import { fetchNewData } from "../api"
 import { useQuery } from "@tanstack/react-query"
+import { ChartDataResponse } from "../types"
+import { ChartKey } from "../constants"
 
 const Blockchain = () => {
   const chartData = useQuery({
@@ -10,22 +12,23 @@ const Blockchain = () => {
 
   if (chartData.isLoading) return "loading..."
 
-  const { response } = chartData.data
+  const { response }: { response: ChartDataResponse } = chartData.data
 
-  const requestApproved = response["Request Approved"]
-  const approvalFailed = response["Approval Failed"]
-
-  const requestProposed = response["Request Proposed"]
-  const proposalFailed = response["Proposal Failed"]
+  const {
+    "Request Approved": requestApproved,
+    "Approval Failed": approvalFailed,
+    "Request Proposed": requestProposed,
+    "Proposal Failed": proposalFailed,
+  } = response
 
   const blockchainData = [
     {
-      title: "propose",
+      title: ChartKey.Propose,
       success: requestProposed,
       failure: proposalFailed,
     },
     {
-      title: "approve",
+      title: ChartKey.Approve,
       success: requestApproved,
       failure: approvalFailed,
     },
@@ -33,8 +36,8 @@ const Blockchain = () => {
 
   return (
     <div className="flex flex-col space-y-4">
-      {blockchainData.map((data) => (
-        <BarChart graphData={data} key={data.title} />
+      {blockchainData.map((item) => (
+        <BarChart graphData={item} key={item.title} />
       ))}
     </div>
   )
