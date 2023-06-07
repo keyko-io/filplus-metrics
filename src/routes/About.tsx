6dee18de895d5f3bbdb6656a2ffd3f9d30b3540d
hcsoylu <hcsoylu.dev@gmail.com>
2023-06-07 18:15:12 +0300

    about page

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,5 +1,85 @@
 const About = () => {
-  return <div>About Page for filplus metrics </div>;
+  return (
+    <div className="flex flex-col">
+      <div className="bg-gradient-to-r from-[#0091ff] to-[#39c1cb] h-[300px] text-white  flex items-center ">
+        <div className="container mx-auto font-bold text-3xl tracking-wide">
+          About Fil+ Metrics
+        </div>
+      </div>
+      <div className="bg-gray-100 min-h-screen">
+        <div className="container mx-auto bg-white rounded-md shadow-lg -translate-y-16 p-5">
+          <div>
+            <h4 className="text-xl font-semibold">About Fil+</h4>
+            <p className="pt-4 text-[15px]">
+              Filecoin Plus is a layer of social trust on top of the Filecoin
+              Network to help incentivize the storage of real data. Root
+              key-holders, notaries, clients, and storage providers, interact
+              through the allocation and spending of DataCap. You can read more
+              about the Fil+ program here.
+            </p>
+            <p className="pt-4 text-[15px]">
+              Clients looking to utilize storage on the network apply to receive
+              DataCap from Notaries, which can be used to incentivize storage
+              providers to make storage deals. Notaries are community-elected
+              trustees responsible for allocating DataCap to parties storing
+              useful data on the network. Storage Providers that receive DataCap
+              receive a 10x boost to their quality-adjusted power for the
+              storage space offered in that deal, which increases their block
+              reward share in the network. This creates a mechanism that
+              incentivizes all participants to make Filecoin more useful.
+            </p>
+            <p className="pt-4 text-[15px]">
+              The goal of this site is to provide metrics, insights, and audit
+              accountability tools for the Filecoin Plus community. You can see
+              health statistics of the overall program, along with tables of
+              data to track the interactions between the stakeholders (Notaries,
+              Clients, and Storage Providers). This site was created by Digital
+              M.O.B. in collaboration with Protocol Labs and the Filecoin
+              Foundation providing development grants.
+            </p>
+          </div>
+          <div>
+            <h4 className="text-xl font-semibold pt-4">Dashboard:</h4>
+            <p className="pt-4 text-[15px]">
+              Filecoin Plus is a layer of social trust on top of the Filecoin
+              Network to help incentivize the storage of real data. Root
+              key-holders, notaries, clients, and storage providers, interact
+              through the allocation and spending of DataCap. You can read more
+              about the Fil+ program here.
+            </p>
+            <p className="pt-4 text-[15px]">
+              The goal of this site is to provide metrics, insights, and audit
+              accountability tools for the Filecoin Plus community. You can see
+              health statistics of the overall program, along with tables of
+              data to track the interactions between the stakeholders (Notaries,
+              Clients, and Storage Providers). This site was created by Digital
+              M.O.B. in collaboration with Protocol Labs and the Filecoin
+              Foundation providing development grants.
+            </p>
+          </div>
+          <div>
+            <h4 className="text-xl font-semibold pt-4">Blockchain:</h4>
+            <p className="pt-4 text-[15px]">
+              Filecoin Plus is a layer of social trust on top of the Filecoin
+              Network to help incentivize the storage of real data. Root
+              key-holders, notaries, clients, and storage providers, interact
+              through the allocation and spending of DataCap. You can read more
+              about the Fil+ program here.
+            </p>
+            <p className="pt-4 text-[15px]">
+              The goal of this site is to provide metrics, insights, and audit
+              accountability tools for the Filecoin Plus community. You can see
+              health statistics of the overall program, along with tables of
+              data to track the interactions between the stakeholders (Notaries,
+              Clients, and Storage Providers). This site was created by Digital
+              M.O.B. in collaboration with Protocol Labs and the Filecoin
+              Foundation providing development grants.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
