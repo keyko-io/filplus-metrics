734a9e057e591ba60808e30c8ec87b7e9893d3a0
Charly López <charly@keyko.io>
2023-09-04 11:18:09 +0200

    Set new eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,12 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended"
+    "plugin:react-hooks/recommended",
+    "standard-with-typescript",
+    "plugin:prettier/recommended",
   ],
   parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  parserOptions: { ecmaVersion: "latest", sourceType: "module", project: "./tsconfig.json" },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": "warn"
