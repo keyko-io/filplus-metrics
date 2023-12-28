d44c1b4e2b275d48f1820a7f7b11177b6c67a2be
Charly López <clriesco@gmail.com>
2023-12-28 09:51:03 +0100

    Change backend url

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const config = {
   serviceUrls: {
     backend:
       import.meta.env.VITE_BACKEND_SERVICE_URI ||
-      "https://fp-core.6omfj573u6naa.us-east-2.cs.amazonlightsail.com/health",
+      "https://fp-core-prod.dp04sa0tdc6pk.us-east-1.cs.amazonlightsail.com/health",
     ldnBot:
       import.meta.env.VITE_LDN_BOT_SERVICE_URI ||
       "https://test.verification.rocks/ldnBotHealthcheck",
@@ -15,6 +15,5 @@ export const config = {
       "https://api.filplus.d.interplanetary.one/public/api/getAllowanceAssignedToLdnV3InLast2Weeks"
   },
   dmobApiKey: import.meta.env.VITE_DMOB_API_KEY || "",
-  ldnBotAuthKey: import.meta.env.VITE_LDN_BOT_AUTH_KEY || "",
-
+  ldnBotAuthKey: import.meta.env.VITE_LDN_BOT_AUTH_KEY || ""
 };
